d6dde5f15fe4e44af3a93850fd64270ca1dc1b73
Alexander Ortiz <aorti017@ucr.edu>
2015-08-15 11:58:06 -0700

    now uses json to find mana cost of each card in deck file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import sys, re
+import sys, re, json
 
 try:
 	deck_file = open(sys.argv[1], 'r')
 except IndexError:
 	print "No file passed"
 	sys.exit(0)
+
+jsonData = open("./res/AllCards.json")
+cardData = json.load(jsonData)
+
 lines = deck_file.readlines()
 for cards in lines:
 	info = re.split('(\d+)', cards.strip())
@@ -12,3 +16,8 @@ for cards in lines:
 	name = info[2]
 	if name[0] == ' ':
 		name = name[1:]
+	try:
+		card_cost = int(cardData[name]["cmc"])
+	except KeyError:
+		#contnues if the card has no cost
+		continue 
