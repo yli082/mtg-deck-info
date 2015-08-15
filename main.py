3cdaf3d0e0d7d5b911f8cff5e015a721b3924247
Alexander Ortiz <aorti017@ucr.edu>
2015-08-15 12:02:04 -0700

    now keeps track of how many crads of each mana cost in deck file

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ except IndexError:
 jsonData = open("./res/AllCards.json")
 cardData = json.load(jsonData)
 
+costArray = [0 for i in range(8)]
+
 lines = deck_file.readlines()
 for cards in lines:
 	info = re.split('(\d+)', cards.strip())
-	num = info[1]
+	num = int(info[1])
 	name = info[2]
 	if name[0] == ' ':
 		name = name[1:]
 	try:
 		card_cost = int(cardData[name]["cmc"])
+		if card_cost > 7:
+			card_cost = 7
+		costArray[card_cost] += num
 	except KeyError:
 		#contnues if the card has no cost
+		costArray[0] += num
 		continue 
+print costArray
