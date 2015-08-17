73e95a24fbc75275b58c7ce8c9e244e2ddfe23bf
Alexander Ortiz <aorti017@ucr.edu>
2015-08-17 08:27:01 -0700

    added sample hand output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, re, json
+import sys, re, json, random
 
 try:
 	deck_file = open(sys.argv[1], 'r')
@@ -10,12 +10,16 @@ jsonData = open("./res/AllCards.json")
 cardData = json.load(jsonData)
 
 costArray = [0 for i in range(8)]
+totalCards = 0
+cardList = []
 
 lines = deck_file.readlines()
 for cards in lines:
 	info = re.split('(\d+)', cards.strip())
 	num = int(info[1])
+	totalCards += num
 	name = info[2]
+
 	if name[0] == ' ':
 		name = name[1:]
 	try:
@@ -27,9 +31,14 @@ for cards in lines:
 		#contnues if the card has no cost
 		if not cardData[name]["type"].startswith("Basic Land"):
 			costArray[0] += num
-		continue 
+	for i in range(0, num):
+		cardList.append(name)
 for i in range(0, len(costArray)):
 	if i == 7:
 		print str(i) + "+: " + str(costArray[i])
 	else:
 		print str(i) + ": " + str(costArray[i])
+
+print "Sample hand:"
+for i in range(0, 6):
+	print cardList[random.randint(0, totalCards-1)]
